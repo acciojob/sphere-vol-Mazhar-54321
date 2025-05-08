6053c72ba7e088703d6f5b6eafaffc609f02ead3
Mazhar-54321 <160312563+Mazhar-54321@users.noreply.github.com>
2025-05-08 12:43:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-function volume_sphere() {
+function volume_sphere(event) {
     //Write your code here
+	event.preventDefault()
+	let radius=document.getElementById("radius").value;
+	if(radius===""){
+		return;
+	}
+	if(!radius.length || Number.isNaN(radius) || Number(radius)<0){
+		document.getElementById("volume").value=NaN
+	}else{
+		let r = parseFloat(radius);
+		document.getElementById("volume").value=((4/3)*(Math.PI)*Math.pow(r,3)).toFixed(4);
+	}
   
 } 
 
